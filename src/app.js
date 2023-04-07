fe2e9b5d46c3b9182c5880952878ddc4be57c1dc
treezyxd <thalesmaltaxd@gmail.com>
2023-04-07 17:38:49 -0300

    create user finished

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
+const users = [];
 
+app.post('/sign-up', (req, res) => {
+  const { username, avatar } = req.body;
+
+  const newUser = {
+    username: username,
+    avatar: avatar
+  };
+  
+  users.push(newUser);
+  res.send('OK');
+});
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`server running at port ${PORT}`));
