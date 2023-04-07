2e97c63881926df3eea7748c7801ac4190651483
treezyxd <thalesmaltaxd@gmail.com>
2023-04-07 18:19:53 -0300

    create tweet finished

--- src/app.js
+++ src/app.js
@@ -4,18 +4,38 @@ const app = express();
 app.use(express.json());
 
 const users = [];
+const tweets = [];
 
 app.post('/sign-up', (req, res) => {
   const { username, avatar } = req.body;
 
   const newUser = {
-    username: username,
-    avatar: avatar
+    username,
+    avatar
   };
   
   users.push(newUser);
   res.send('OK');
 });
+
+app.post('/tweets', (req, res) => {
+  const { username, tweet } = req.body;
+
+  if (users.find(user => user.username !== username)) {
+    res.status(401).send("UNAUTHORIZED");
+    return;
+  }
+
+  const newTweet = {
+    username,
+    tweet
+  };
+
+  tweets.push(newTweet);
+  res.status(201).send("OK");
+});
+
+
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`server running at port ${PORT}`));
