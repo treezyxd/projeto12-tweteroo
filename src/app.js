af5055c68cdc9b9601d0338cfa420abefe59a022
treezyxd <thalesmaltaxd@gmail.com>
2023-04-08 02:28:47 -0300

    get tweets finished

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const users = [];
@@ -13,9 +15,15 @@ app.post('/sign-up', (req, res) => {
     username,
     avatar
   };
-  
+
+  if(!username || !avatar) {
+    res.status(401).send('Todos os campos são obrigatórios!');
+    return;
+  }
+
   users.push(newUser);
-  res.send('OK');
+  res.status(201).send('OK');
+  
 });
 
 app.post('/tweets', (req, res) => {
@@ -35,7 +43,21 @@ app.post('/tweets', (req, res) => {
   res.status(201).send("OK");
 });
 
+app.get('/tweets', (req, res) => {
+  const lastTweets = tweets.slice(-10);
 
+  if(lastTweets.length > 10) {
+    lastTweets = lastTweets.slice(lastTweets.length - 10);
+  }
+
+  const avatars = lastTweets.map(tweet => {
+    const user = users.find(user => user.username === tweet.username);
+    const avatar = user ? user.avatar : null;
+    return { ...tweet, avatar, username: tweet.username, tweet: tweet.tweet };
+  });
+
+  res.status(200).send(avatars);
+});
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`server running at port ${PORT}`));
